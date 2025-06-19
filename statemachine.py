5a87b675cdfff04b775056303be170a1ed931759
rudolf.steiner <tigerfei@gmail.com>
2025-06-19 18:51:16 -0400

    v1 State Pattern

--- statemachine.py
+++ statemachine.py
@@ -36,9 +36,10 @@ class S2State(State):
 class FiniteStateMachine:
     _context = None # handle the state pattern
 
-    def __init__(self, context, alphabet):
-        self._context = context
-        self._alphabet = alphabet
+    def __init__(self, context, alphabet, end_states):
+        self._context = context # Finite States and Initial state using state pattern
+        self._alphabet = alphabet # alphabet
+        self._end_states = end_states # end states
 
     @abstractmethod
     def handle(self, cargo):
@@ -46,24 +47,29 @@ class FiniteStateMachine:
 
 
 class D3FSM(FiniteStateMachine):
-    def __init__(self, context=Context(S0State()), alphabet=("0", "1"), logger=None):
-        super().__init__(context, alphabet)
-        self.stateValue = {"S0State": 0, "S1State": 1, "S2State": 2}
-        self.logger = logger
+    def __init__(self, context=Context(S0State()), alphabet=("0", "1"), end_states=None, logger=None):
+        super().__init__(context, alphabet, end_states)
+        self._stateValue = {"S0State": 0, "S1State": 1, "S2State": 2} # map from final state to output
+        self._logger = logger
 
     def handle(self, cargo: str):
+        """
+        implementation of mod by 3
+        :param cargo: input string
+        :return: result
+        """
         # check empty input
         if len(cargo) == 0:
-            if self.logger:
-                self.logger.error(f"Empty Input")
+            if self._logger:
+                self._logger.error(f"Empty Input")
             print(f"Error: Empty Input")
             return False  # Invalid input symbol
 
         # check any non-1/0 char
         for i in range(len(cargo)):
             if cargo[i] not in self._alphabet:
-                if self.logger:
-                    self.logger.error(f"input at index {str(i)} is not in alphabet {str(self._alphabet)} list")
+                if self._logger:
+                    self._logger.error(f"input at index {str(i)} is not in alphabet {str(self._alphabet)} list")
                 print(
                     f"Error: input -- cargo at index {str(i + 1)} is not in alphabet {str(self._alphabet)} list")
                 return False  # Invalid input symbol
@@ -71,13 +77,12 @@ class D3FSM(FiniteStateMachine):
         for char in cargo:
             self._context.handle(int(char))
 
-        return self.stateValue[type(self._context.state).__name__]
+        return self._stateValue[type(self._context.state).__name__]
 
 
     def reset(self):
         """
-        back to the initial stage S0State
-        :return:
+        back to the initial state S0State
         """
         self._context.set_state(S0State())
 
